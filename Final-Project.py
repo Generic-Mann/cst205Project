19f25586c5f4a98cc438c6845b211011d0ea77e6
Michael Garcia <157657140+Generic-Mann@users.noreply.github.com>
2025-05-11 17:50:22 -0700

    Finished the beautiful soup portion

--- Final-Project.py
+++ Final-Project.py
@@ -10,14 +10,15 @@ soup = BeautifulSoup(site_html.read(), 'lxml')
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-images = soup.findAll('img')
-paragraphs = soup.findAll('p')
-texts = ' '.join(p.get_text() for p in paragraphs)
+raw_images = [img['src'] for img in soup.find_all('img') if img.get('src')]
+images = [src if src.startswith('http') else my_site + src for src in raw_images]
 
+for link in soup.find_all('img'):
+    print(link.get('src'))
+
+texts = [p.get_text(strip=True) for p in soup.find_all('p')]
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
 
     return render_template('soup.html', images=images, texts=texts)
-
-    
