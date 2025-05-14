e8f43b4edb9235420ef0835c1f5a096a7254406d
OscarAvilSal <oaviles-saldana@csumb.edu>
2025-05-14 11:13:10 -0700

    Update Final-Project.py

--- Final-Project.py
+++ Final-Project.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, request
 from flask_bootstrap import Bootstrap5
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
+
 
 my_site='https://csumb.edu/'
 site_html=urlopen(my_site)
@@ -9,7 +11,7 @@ soup = BeautifulSoup(site_html.read(), 'lxml')
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-
+# web scraping
 raw_images = [img['src'] for img in soup.find_all('img') if img.get('src')]
 images = [src if src.startswith('http') else my_site + src for src in raw_images]
 
@@ -17,8 +19,25 @@ for link in soup.find_all('img'):
     print(link.get('src'))
 
 texts = [p.get_text(strip=True) for p in soup.find_all('p')]
-
-@app.route('/', methods=('GET', 'POST'))
+print(texts)
+# routes 
+@app.route('/')
 def index():
+
+    return render_template('landing_page.html')
+
+@app.route('/text_to_speech.html', methods=['GET', 'POST'])
+def tts():
+    my_site = request.form.get('user_input')
+    return render_template('text_to_speech.html', user_input=my_site)
+
+
+@app.route('/colorblind_value_converter.html')
+def cvc():
+
+    return render_template('Colorblind_value_converter.html')
+
+@app.route('/soup', methods=('GET', 'POST'))
+def so():
 
     return render_template('soup.html', images=images, texts=texts)
