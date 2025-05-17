58a8012b4c6dbab19b17a972b9a5d66bd9a791d2
Oscar-AS <oaviles-saldana@csumb.edu>
2025-05-16 23:44:25 -0700

    update

--- final_project.py
+++ final_project.py
@@ -7,6 +7,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from datetime import datetime
+from color import use
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
@@ -15,6 +17,9 @@ bootstrap = Bootstrap5(app)
 Links = None
 
 def webscrape(web):
+#     Beautiful Soup
+# web scraps a website given by the user and displays
+# the images and text of the page.
     my_site=str(web)
     site_html=urlopen(my_site)
     soup = BeautifulSoup(site_html.read(), 'lxml')
@@ -57,13 +62,25 @@ def index():
 #     return render_template('colorblind_value_converter.html')
 
 
+# Beautiful Soup
+# web scraps a website given by the user and displays
+# the images and text of the page.
 @app.route('/soup', methods=('GET', 'POST'))
 def so():
     if request.method == 'POST':
         if request.form.get('user_link') is None:
             return redirect(url_for('soup'))
+        use()
         
         
 
     return render_template('soup.html', images=images, texts=texts)
+
+
+# Course: CST205 - Multimedia Design & Programming
+# Roles:
+# Michael Garcia: Beautiful Soup
+# Christopher Morales: Colorblind Filter
+# Valeria Arteaga-Higueros: design
+# Oscar Aviles-Saldana: Landing Page & UI
 
